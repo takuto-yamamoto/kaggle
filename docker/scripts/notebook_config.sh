6bab14f287426b973895f21f954823db488779de
takuto-yamamoto <tyguitar0v0627@gmail.com>
2023-02-21 22:01:51 +0900

    [fix]typo

--- docker/scripts/notebook_config.sh
+++ docker/scripts/notebook_config.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 hashed_passwd=$1
-config=~/.jupyter/jupyter_notebook_config.py
+config=/root/.jupyter/jupyter_notebook_config.py
 
 mv "$config" "$config".old
 
-cat $config.old | \
+cat "$config".old | \
 	sed "s/^#c.NotebookApp.ip = 'localhost'/c.NotebookApp.ip = '*'/g" | \
 	sed "s/^#c.NotebookApp.open_browser = True/c.NotebookApp.open_browser = False/g" | \
 	sed "s/^#c.NotebookApp.port = 8888/c.NotebookApp.port = 8888/g" | \
